a5b11664cc2781092e1d95f992bbef6c8be3bd54
Rodney Geerlings <rodney@rgeerlings.com>
2016-10-10 22:26:05 +0200

    Index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ date_default_timezone_set('Europe/Amsterdam');
 $today = date("Y-m-d");
 require "includes/header.php";
 
+
 ?>
     <br>
     <h1>Clientkaart</h1>
@@ -22,58 +23,58 @@ require "includes/header.php";
         </tr>
     </table>
 
-    <?php
-         $query = "SELECT * FROM Card";
-         $result = mysqli_query($conn,$query);
-         while ($row = $result->fetch_array(true))
-         
-     ?>
 
-        <table class="table table-hover table-bordered table-big">
-            <tr>
-                <th>Dag</th>
-                <th>Datum</th>
-                <th>Tijd</th>
-                <th>Gewicht KG</th>
-                <th>BMI</th>
-                <th>Vet %</th>
-                <th>Visc.vet</th>
-                <th>Spiermassa</th>
-                <th>MO</th>
-            </tr>
 
-            <tr>
-                <th>
-                    <?=$prepath?>
-                </th>
-                <th>
-                    <?=$prepath?>
-                </th>
-                <th>
-                    <?=$prepath?>
-                </th>
-                <th>
-                    <?=$prepath?>
-                </th>
-                <th>
-                    <?=$prepath?>
-                </th>
-                <th>
-                    <?=$prepath?>
-                </th>
-                <th>
-                    <?=$prepath?>
-                </th>
-                <th>
-                    <?=$prepath?>
-                </th>
-                <th>
-                    <?=$prepath?>
-                </th>
-            </tr>
-
-        </table>
-
+    <table class="table table-hover table-bordered table-big">
+        <tr>
+            <th>Dag</th>
+            <th>Datum</th>
+            <th>Tijd</th>
+            <th>Gewicht KG</th>
+            <th>BMI</th>
+            <th>Vet %</th>
+            <th>Visc.vet</th>
+            <th>Spiermassa</th>
+            <th>MO</th>
+        </tr>
         <?php
+      
+                    $query = "SELECT * FROM Card";
+                    $result = mysqli_query($conn,$query);
+                    while ($row = $result->fetch_array(true));  
+                  
+        ?>
+            <tr>
+                <td>
+                    <?=$row['Dag']?>
+                </td>
+                <td>
+                    <?=$row['Datum']?>
+                </td>
+                <td>
+                    <?=$row['Tijd']?>
+                </td>
+                <td>
+                    <?=$row['Gewicht']?>
+                </td>
+                <td>
+                    <?=$row['BMI']?>
+                </td>
+                <td>
+                    <?=$row['Vet']?>
+                </td>
+                <td>
+                    <?=$row['Viceraalvet']?>
+                </td>
+                <td>
+                    <?=$row['Spiemassa']?>
+                </td>
+                <td>
+                    <?=$row['MO']?>
+                </td>
+            </tr>
+            <!--  Footer  -->
+
+            <?php
 require "includes/footer.php";
 ?>

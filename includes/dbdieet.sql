fc4bcaa5ed36a621cfc94304b5db5eaa72ef53b8
Rodney Geerlings <rodney@rgeerlings.com>
2016-11-01 21:42:14 +0100

    update
    value

--- includes/dbdieet.sql
+++ includes/dbdieet.sql
@@ -42,12 +42,13 @@ CREATE TABLE IF NOT EXISTS `Card` (
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 --
--- Dumping data for table `Card`
+-- Dumping data for table `Card` http://www.vetpercentagemeten.nl/
 --
 
 INSERT INTO `Card` (`Dag`, `Datum`, `Tijd`, `Gewicht`, `BMI`, `Vet`, `Visceraalvet`, `Spiermassa`, `MO`) VALUES
 ('Di', '2016-08-01', '12:15:00', 106.6,39.9,37.5,20,30.2,''),
-('Di', '2016-09-21', '12:15:00', 104.7,38.9,36.4,20,30.2,'');
+('Di', '2016-09-21', '12:15:00', 104.7,38.9,36.4,20,30.2,''),
+('Di', '2016-10-21', '12:15:00', 101.6,36.9,32.48,20,30.2,'');
 
 
 
